976fa21cbb6f395bd82d2a2bf6cab6458afe68a8
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2016-07-25 22:17:34 +0300

    CWebDownloader: QByteArray for data buffer, exponential size growth to reduce the number of allocations

--- src/cwebdownloader.cpp
+++ src/cwebdownloader.cpp
@@ -7,27 +7,24 @@ DISABLE_COMPILER_WARNINGS
 RESTORE_COMPILER_WARNINGS
 
 struct MemoryStruct {
-  char *memory;
-  size_t size;
+	QByteArray dataBuffer;
+	size_t bytesUsed = 0;
 };
 
 size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
-  size_t realsize = size * nmemb;
-  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+	const size_t newChunkSize = size * nmemb;
+	MemoryStruct* mem = (MemoryStruct*) userp;
 
-  mem->memory = (char*)realloc(mem->memory, mem->size + realsize + 1);
-  if(mem->memory == NULL) {
-	/* out of memory! */
-	printf("not enough memory (realloc returned NULL)\n");
-	return 0;
-  }
+	const size_t oldBufferSize = mem->dataBuffer.size();
+	const size_t requestedSize = mem->bytesUsed + newChunkSize;
+	if (oldBufferSize < requestedSize)
+		mem->dataBuffer.resize((int) std::max(requestedSize, oldBufferSize * 3 / 2)); // Increasing the size 1.5x times to reduce the number of allocations for scenarios with many chunks
 
-  memcpy(mem->memory + mem->size, contents, realsize);
-  mem->size += realsize;
-  mem->memory[mem->size] = 0;
+	memcpy(mem->dataBuffer.data() + mem->bytesUsed, contents, newChunkSize);
+	mem->bytesUsed += newChunkSize;
 
-  return realsize;
+	return newChunkSize;
 }
 
 CWebDownloader::CWebDownloader()
@@ -40,10 +37,8 @@ QByteArray CWebDownloader::download(const QString &url)
 	CURL *curl_handle;
 	CURLcode res;
 
-	struct MemoryStruct chunk;
-
-	chunk.memory = (char*)malloc(1);  /* will be grown as needed by the realloc above */
-	chunk.size = 0;    /* no data at this point */
+	MemoryStruct chunk;
+	chunk.dataBuffer.resize(16384); // Meaningful starting size of 16K
 
 	curl_global_init(CURL_GLOBAL_ALL);
 
@@ -57,7 +52,7 @@ QByteArray CWebDownloader::download(const QString &url)
 	curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
 
 	/* we pass our 'chunk' struct to the callback function */
-	curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+	curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *) &chunk);
 
 	/* some servers don't like requests that are made without a user-agent
 	   field, so we provide one */
@@ -67,28 +62,16 @@ QByteArray CWebDownloader::download(const QString &url)
 	res = curl_easy_perform(curl_handle);
 
 	/* check for errors */
-	QByteArray result;
 	if (res != CURLE_OK)
 		qDebug() << "curl_easy_perform() failed:" << curl_easy_strerror(res);
 	else
-	{
-	  /*
-	   * Now, our chunk.memory points to a memory block that is chunk.size
-	   * bytes big and contains the remote file.
-	   *
-	   * Do something nice with it!
-	   */
-
-		result = QByteArray(chunk.memory, (int)chunk.size);
-	}
+		chunk.dataBuffer.truncate((int) chunk.bytesUsed);
 
 	/* cleanup curl stuff */
 	curl_easy_cleanup(curl_handle);
 
-	free(chunk.memory);
-
 	/* we're done with libcurl, so clean it up */
 	curl_global_cleanup();
 
-	return result;
+	return res == CURLE_OK ? chunk.dataBuffer : QByteArray();
 }
