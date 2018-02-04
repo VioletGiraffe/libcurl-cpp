52152bf677878f3c9c6fbefd79a730aa3fc6c1e9
VioletGiraffe <alexxxx89@ya.ru>
2018-02-04 20:33:37 +0200

    Default user agent updated to Firefox 58.0

--- src/cwebdownloader.h
+++ src/cwebdownloader.h
@@ -13,7 +13,7 @@ class LIBCURL_CPP_EXPORT CWebDownloader
 {
 public:
 	// This class is managing some resources and the instance is meant to be kept around if you need to download multiple URLs (as opposed to creating a new instance each time).
-	explicit CWebDownloader(const QString& userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36");
+	explicit CWebDownloader(const QString& userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0");
 	~CWebDownloader();
 
 	void setUserAgent(const QString& userAgent);
